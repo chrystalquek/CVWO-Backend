57bb64ecf39166c62702d68da594b5ab53037586
chrystalquek <chrystalquek@yahoo.com.sg>
2020-01-12 21:56:59 +0800

    api

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope '/api' do
+    post "/login", to: "auth#login"
+    get "/auto_login", to: "auth#auto_login"
     resources :users, only: [:create] do
       resources :todos
     end
   end
-   post "/login", to: "auth#login"
-   get "/auto_login", to: "auth#auto_login"
+  #  post "/login", to: "auth#login"
+  #  get "/auto_login", to: "auth#auto_login"
    
 end
 
