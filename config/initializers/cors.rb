a732031b32a16308635679e2ebf90f70a8e8f366
chrystalquek <chrystalquek@yahoo.com.sg>
2020-01-13 10:06:52 +0800

    netlify

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -18,7 +18,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
     allow do
-      origins 'https://sharp-roentgen-6f32fa.netlify.com/'
+      origins '*'
     
       resource '*',
         headers: :any,
