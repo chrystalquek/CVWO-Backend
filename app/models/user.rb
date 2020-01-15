08b5441df2114a3495757589e2105459afe842e8
chrystalquek <chrystalquek@yahoo.com.sg>
2020-01-15 17:15:26 +0800

    validation only when create

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     validates :username, presence: true
     validates :username, uniqueness: true
-    validates :password, length: { minimum: 4 }
+    validates :password, length: { minimum: 4 }, on: :create
     validates :email, presence: true
     validates :email, uniqueness: true
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
