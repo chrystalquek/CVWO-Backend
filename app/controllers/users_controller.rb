dbe41bb37ef86dd141898891254caae1830b842a
chrystalquek <chrystalquek@yahoo.com.sg>
2020-01-19 04:20:29 +0800

    require_login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+class UsersController < AuthController
   # before_action :set_user, only: [:show, :update, :destroy]
   before_action :require_login, except: :create
 
